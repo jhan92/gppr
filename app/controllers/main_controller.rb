20ccf57fdc93115c5d5c08b63540f00995480d32
James Han <james.han@workday.com>
2016-10-17 11:47:00 -0700

    reading from google drive

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,6 +1,14 @@
 class MainController < ApplicationController
+  require "google_drive"
   
   def index
     @test = 'text'
+    
+    session = GoogleDrive::Session.from_config("config.json")
+    ws = session.spreadsheet_by_key("1A8cUX-nHnyXOcIxuhKMcZm1Wu7MZtlrTV8UZzZ6nCDI").worksheets[0]
+    
+    @test = ws[1,1]
+    
+    
   end
 end
