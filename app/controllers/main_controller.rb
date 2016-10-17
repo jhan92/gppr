63b1ce3dbe76e718f1b2276d9156c69f62f7ae0e
James Han <james.han@workday.com>
2016-10-17 12:22:32 -0700

    intiial reading of impact

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,13 +1,19 @@
 class MainController < ApplicationController
   require "google_drive"
+      
+  DESC_COL = 2
+  VOLUNTEER_TOTAL_COL = 4
+    
   
   def index
     @test = 'text'
     
     session = GoogleDrive::Session.from_config("config.json")
     ws = session.spreadsheet_by_key("1A8cUX-nHnyXOcIxuhKMcZm1Wu7MZtlrTV8UZzZ6nCDI").worksheets[0]
-    
-    @test = ws[1,1]
+
+    @impact = {}
+    @impact['desc'] = ws[6, DESC_COL]
+    @impact['num'] = ws[6, VOLUNTEER_TOTAL_COL]
     
     
   end
