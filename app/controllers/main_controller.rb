925686210b296f6e2ac5bff2af2307bcdec5c403
James Han <james.han@workday.com>
2016-10-17 12:29:33 -0700

    adding title for testing

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -10,6 +10,7 @@ class MainController < ApplicationController
     
     session = GoogleDrive::Session.from_config("config.json")
     ws = session.spreadsheet_by_key("1A8cUX-nHnyXOcIxuhKMcZm1Wu7MZtlrTV8UZzZ6nCDI").worksheets[0]
+    @title = ws[1,1]
 
     @impact = {}
     @impact['desc'] = ws[6, DESC_COL]
